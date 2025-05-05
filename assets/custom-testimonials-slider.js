736c3826f64c424d4229a7638bf3bdfbbf82bfcd
azure <azure.ad@yahoo.com>
2025-05-06 03:43:51 +0800

    blocks rendered dynamically, and slider initialized

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -1,5 +1,15 @@
 // initialize slideshow
 
+const slider_html = document.querySelector('.testimonial-slider');
+
+const slider = new Flickity(slider_html,{
+    pageDots: false,
+    autoPlay: false,
+    wrapAround: true,
+    cellAlign: 'left',
+    pauseAutoPlayOnHover: false
+});
+
 // config:
 // assume there is just one single slider on the page
 // navigation dots should be disabled
